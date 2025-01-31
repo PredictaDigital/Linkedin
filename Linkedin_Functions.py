64af2de29a827e95ad985dc9e30e31d24368217c
PredictaDigital <analytics@predictadigital.com>
2025-01-31 12:30:18 +0530

    Update Linkedin_Functions.py

--- Linkedin_Functions.py
+++ Linkedin_Functions.py
@@ -2,30 +2,36 @@ import requests
 import pyodbc
 
 # LinkedIn API credentials
-CLIENT_ID = '86gawjaqysdvxi'
-CLIENT_SECRET = 'JfWNGh1yETThGlVy'
-ACCESS_TOKEN = ('AQW0NwO3zqAl-3Y0AeJKrbTSFNYxANRUHPteYvxIzCSl9gMjTLvxHne-z-a39PhVNF0xfuuxSJd_OGbalP_OZrVDz56OjlbeTW7rxZByml3jCg0pVnK8r6jy3oq9ZxkcZjAuKcZVYXos4sXc7HX7DmlJj3bfk5Ml8HpeTBumj9DWRDwUjWN1TWnfUULHk9L6aek6_Gs2GrwXzoMQSNVqqG9-KMRqBAw0qrBqCyyf3TJtl7Z5mf3WQePdIb_51RkvebNVAdsMqzB5_u9zHABxPw7utgtG0Yo771INJ0TFCzfJysr4_lgsyPbYBRSEuVjznun2X7wBH8m5q8eWZlnmeA_DDdnK2g')
+APP_VERSION = '202411'
+ACCESS_TOKEN = (
+    'AQUhGyfzqf6s5zDC4ganhgSd2rH5vsBER_-mEm95QGwCMNxX6fUsAn_JFHpH8Rf6U5aPR8HkcA_g-MSwFCqSIMRC9bcX5dkHRBvCp1fwmFDA2vtZ7g-NxstC8b6PXMHBdHJLqiMiQj0C6f1bPkO0zGBWMdU3LOSi1c2hA5X-upa4MK-y1LyWVpe9XbNMBUGwvS7gB7U4Ivbeq5-lv7tfpmfckYh2FjImtZIG4B8JHKkaqAYrxn9xK1_A2L-Rds8eeJQB0Z7tZb-yFolupHuzTeu_nF6vqZ3OWD4MpNhyDwsNaz2JB9fTXgcTIUPxFLVxa__jOxaOr8whb5f0UdwJ_S_2pCLMBw')
 
 # Define the API endpoint for retrieving engagement metrics
 analytics_url = f'https://api.linkedin.com/v2/functions'
 
 headers = {'Authorization': f'Bearer {ACCESS_TOKEN}',
            'Content-Type': 'application/json',
-            'LinkedIn-Version': '202304'
-            # 'X-Restli-Protocol-Version': '2.0.0'
+            'LinkedIn-Version': f'{APP_VERSION}'
            }
 # Retrieve profile information from LinkedIn API
 response = requests.get(analytics_url, headers=headers)
 analytics_data = response.json()
 
-print(analytics_data)
+# print(analytics_data)
 # Connect to SQL Server
-connection_string = 'DRIVER={SQL Server};SERVER=Predicta.Database.Windows.Net;DATABASE=Predicta;UID=PredictaAdmin;PWD=Yhf^43*&^FHHytf'
-conn = pyodbc.connect(connection_string)
+server = 'Predicta.Database.Windows.Net'
+database = 'Predicta'
+username = 'PredictaAdmin'
+password = 'Yhf^43*&^FHHytf'
+db_table = "dbo.Linkedin_Functions_KS"  # need to change this to Fact table
+conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}')
 cursor = conn.cursor()
 
+# Truncate the table
+cursor.execute(f'TRUNCATE TABLE {db_table}')
+
 # Define the INSERT query for Countries
-insert_query = "INSERT INTO dbo.Linkedin_Functions_KS (URN,Function_ID,Function_Name) VALUES (?, ?, ?)"
+insert_query = f"INSERT INTO {db_table} (URN,Function_ID,Function_Name) VALUES (?, ?, ?)"
 
 # Iterate over Countries and insert each record into the SQL table
 for element in analytics_data['elements']:
